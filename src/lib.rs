d9fb1218bd91813e124fc14268edf297d00fee11
David Waite <d.waite1986@gmail.com>
2023-06-27 21:35:47 +1200

    Refactored weapon die system for better scalability

--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,72 @@
 use rand::Rng;
 use rand::rngs::ThreadRng;
 
-const ROLL_MIN: i32 = 1;
-const ROLL_MAX: i32 = 21;
+const ROLL_MIN: u8 = 1;
+const ROLL_MAX_D20: u8 = 21;
+
+#[derive(Debug)]
+pub struct Weapon {
+    number_die: u8,
+    die_size: u8,
+    max_roll: u8,
+}
+
+impl Weapon {
+
+    pub fn new(number_die: u8, die_size: u8) -> Weapon {
+        Weapon {
+            number_die: number_die,
+            die_size: die_size,
+            max_roll: die_size + 1,
+        }
+    }
+
+    fn roll_damage(&self, dice_roller: &mut ThreadRng) -> u8 {
+
+        let mut dmg_roll = 0;
+        for i in 0..self.number_die {
+            dmg_roll += dice_roller.gen_range(ROLL_MIN..self.max_roll)
+        }
+
+        dmg_roll
+    }
+}
 
 #[derive(Debug)]
 pub struct TurnSimulation {
-    number_attacks: i32,
-    weapon_die: i32,
-    hit_modifier: i32,
-    damage_modifier: i32,
-    damage_roll_max: i32,
+    number_attacks: u8,
+    hit_modifier: u8,
+    damage_modifier: u8,
+    weapon_type: Weapon,
     modifier_options: Vec<bool>,
     dice_roller: ThreadRng,
 }
 
 impl TurnSimulation {
 
-    pub fn new(number_attacks: i32, weapon_die: i32, hit_modifier: i32, damage_modifier: i32) -> TurnSimulation {
+    pub fn new(number_attacks: u8, hit_modifier: u8, damage_modifier: u8, weapon_type: Weapon) -> TurnSimulation {
         TurnSimulation {
             number_attacks: number_attacks,
-            weapon_die: weapon_die,
-            damage_roll_max: weapon_die + 1,
             hit_modifier: hit_modifier,
             damage_modifier: damage_modifier,
+            weapon_type: weapon_type,
             modifier_options: Vec::<bool>::new(),
             dice_roller: rand::thread_rng(),
         }
     }
 
-    fn roll_attack(&mut self) -> i32 {
-        self.dice_roller.gen_range(ROLL_MIN..ROLL_MAX) + self.hit_modifier
+    fn roll_attack(&mut self) -> u8 {
+        self.dice_roller.gen_range(ROLL_MIN..ROLL_MAX_D20) + self.hit_modifier
     }
 
-    fn roll_damage(&mut self) -> i32 {
-        self.dice_roller.gen_range(ROLL_MIN..self.weapon_die + 1) + self.damage_modifier
+    fn roll_weapon(&mut self) -> u8 {
+        self.weapon_type.roll_damage(&mut self.dice_roller)
     }
 
-    pub fn roll(&mut self, target_ac: i32) -> i32 {
+    pub fn roll(&mut self, target_ac: u8) -> u8 {
 
         match self.roll_attack() >= target_ac {
-            true => self.roll_damage(),
+            true => self.roll_weapon() + self.damage_modifier,
             false => 0
         }
     }
@@ -50,49 +76,76 @@ impl TurnSimulation {
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_roll_attack() {
+    fn unpack_roll_vector(roll_capture: &Vec<u8>) -> (u8, u8) {
 
-        let mut ts = TurnSimulation::new(0, 0, 0, 0);
-        let mut roll_capture: Vec<i32> = Vec::new();
-    
-        for _ in 0..10000 {
-            roll_capture.push(
-                ts.roll_attack()
-            );
-        }
-    
-        assert_eq!(
-            *roll_capture.iter().min().unwrap(),
-            ROLL_MIN
-        );
-        assert_eq!(
-            *roll_capture.iter().max().unwrap(),
-            ROLL_MAX - 1
-        );
+        let obs_min = *roll_capture.iter().min().unwrap();
+        let obs_max = *roll_capture.iter().max().unwrap();
+
+        (obs_min, obs_max)
     }
 
+    // Weapon
     #[test]
     fn test_roll_damage() {
 
         let damage_die = 8;
+        let mut roller = rand::thread_rng();
+        let w = Weapon::new(1, damage_die);
 
-        let mut ts = TurnSimulation::new(0, damage_die, 0, 0);
-        let mut roll_capture: Vec<i32> = Vec::new();
-
+        let mut roll_capture: Vec<u8> = Vec::new();
         for _ in 0..10000 {
-            roll_capture.push(
-                ts.roll_damage()
-            );
+            roll_capture.push(w.roll_damage(&mut roller));
         }
-    
-        assert_eq!(
-            *roll_capture.iter().min().unwrap(),
-            ROLL_MIN
-        );
-        assert_eq!(
-            *roll_capture.iter().max().unwrap(),
-            damage_die
-        );
+
+        let (obs_min, obs_max) = unpack_roll_vector(&roll_capture);
+        assert_eq!(obs_min, ROLL_MIN);
+        assert_eq!(obs_max, damage_die);
+    }
+
+    // TurnSimulation
+    #[test]
+    fn test_roll_attack() {
+
+        let mut ts = TurnSimulation::new(0, 0, 0, Weapon::new(0, 0));
+
+        let mut roll_capture: Vec<u8> = Vec::new();    
+        for _ in 0..10000 {
+            roll_capture.push(ts.roll_attack());
+        }
+
+        let (obs_min, obs_max) = unpack_roll_vector(&roll_capture);
+        assert_eq!(obs_min, ROLL_MIN);
+        assert_eq!(obs_max, ROLL_MAX_D20 - 1);
+    }
+
+    #[test]
+    fn test_roll_weapon() {
+
+        let damage_die = 8;
+        let mut ts = TurnSimulation::new(0, 0, 0, Weapon::new(1, damage_die));
+        let mut roll_capture: Vec<u8> = Vec::new();    
+        for _ in 0..10000 {
+            roll_capture.push(ts.roll_weapon());
+        }
+
+        let (obs_min, obs_max) = unpack_roll_vector(&roll_capture);
+        assert_eq!(obs_min, ROLL_MIN);
+        assert_eq!(obs_max, damage_die);
+    }
+
+    #[test]
+    fn test_roll_succeed() {
+
+        let mut ts = TurnSimulation::new(0, 1, 0, Weapon::new(1, 6));
+        let roll_damage = ts.roll(0);
+        assert!(roll_damage > 0);
+    }
+
+    #[test]
+    fn test_roll_failed() {
+
+        let mut ts = TurnSimulation::new(0, 0, 0, Weapon::new(1, 6));
+        let roll_damage = ts.roll(21);
+        assert_eq!(roll_damage, 0);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use dpr_simulator::TurnSimulation;
+use dpr_simulator::{TurnSimulation,Weapon};
 
 fn main() {
-    println!("Hello, world!");
 
-    let mut ts = TurnSimulation::new(1, 8, 0, 0);
+    // Standard 1d8 weapon, and a 1st level PC with +3 in main stat
+    let weapon = Weapon::new(1, 8);
+    let mut ts = TurnSimulation::new(1, 5, 3, weapon);
     let target_ac = 12;
 
     let x = ts.roll(target_ac);
