047c154fe7b0a394ef51e411125ba3207f9a918c
David Waite <d.waite1986@gmail.com>
2023-12-28 22:12:16 +1300

    Re-implemented front end

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use dpr_simulator::Ruleset;
+use polars::frame::DataFrame;
 
 fn main() {
     // Entry point for the tool interface
@@ -12,55 +13,58 @@ fn main() {
         false => Ruleset::DND5e,
     };
 
-    // MAP...
-
     // Process the information and capture results as a polars DataFrame
-    let output_df = match dpr_simulator::process_simulation(
+    let mut output_df = dpr_simulator::process_simulation(
         cli.ac_targets,
         cli.to_hit,
         cli.weapon_details,
         ruleset,
         cli.number_turns,
-    ) {
-        Ok(df) => df,
-        Err(e) => {
-            report_error(&e.to_string());
-            std::process::exit(1);
-        }
-    };
+    );
 
-    // Store the output
-    match dpr_simulator::write_to_parquet(&cli.output, output_df) {
-        Ok(_) => println!("Completed! Results written to file '{}'!", cli.output),
-        Err(e) => {
-            report_error(&e.to_string());
-            std::process::exit(1);
-        }
-    };
+    // Store the output if required
+    if let Some(output_path) = cli.output {
+        store_output(&output_path, &mut output_df);
+    }
+
+    /* Report the summary results to the user. Currently it appears that the display
+        can only be customised using env variables:
+            POLARS_FMT_TABLE_HIDE_COLUMN_DATA_TYPES (hide data types)
+            POLARS_FMT_TABLE_HIDE_COLUMN_SEPARATOR (hide separator)
+    */
+    let summary_df = dpr_simulator::summarise_results(output_df);
+    println!("{}", summary_df);
 }
 
-fn report_error(error_msg: &str) {
-    println!("ERROR: {}", error_msg);
+fn store_output(output_path: &str, output_df: &mut DataFrame) -> () {
+    match dpr_simulator::write_to_parquet(&output_path, output_df) {
+        Ok(_) => println!("Completed! Results written to file '{}'!", output_path),
+        Err(e) => {
+            println!("ERROR: {}", e);
+            std::process::exit(1);
+        }
+    }   
 }
 
 #[derive(Parser)]
 struct Cli {
     // Build the CLI input arguments and options.
-    /// To-Hit modifier, one or one per attack to be made
-    #[arg(short = 't', long, value_name = "TO HIT", num_args = 1.., value_delimiter = ' ')]
-    to_hit: Vec<i32>,
 
     /// Space-delimited AC values to test against
     #[arg(short, long, value_name = "AC TARGETS", num_args = 1.., value_delimiter = ' ', default_values_t = vec![12, 14, 16, 18, 20])]
     ac_targets: Vec<i32>,
 
-    /// Details of each attack to be made in the form 1d8+5
+    /// Details of the attack roll in the form 1d20+X
+    #[arg(short = 't', long, value_name = "TO HIT", num_args = 1.., value_delimiter = ' ')]
+    to_hit: Vec<String>,
+
+    /// Details of each attack to be made in the form 1dX+Y or 1dX,1dY+Z
     #[arg(short = 'w', long, value_name = "WEAPON DETAILS", num_args = 1.., value_delimiter = ' ')]
     weapon_details: Vec<String>,
 
-    /// Path to save results (Apache parquet format)
+    /// Path to save results in Apache parquet format (optional)
     #[arg(short, long, value_name = "OUTPUT FILE")]
-    output: String,
+    output: Option<String>,
 
     /// Number of turns to simulate
     #[arg(short, long, value_name = "NUMBER TURNS", default_value_t = 1_000_000)]
@@ -69,8 +73,4 @@ struct Cli {
     /// Use Pathfinder 2e rules for critical hits and damage calculation
     #[arg(long, default_value_t = false)]
     use_pf2e_criticals: bool,
-
-    /// Apply a Pathfinder 2e MAP progression to the attacks (optional, NOT YET IMPLEMENTED)
-    #[arg(long, value_name = "MAP SEQUENCE", num_args = 1.., value_delimiter = ' ')]
-    map_sequence: Option<Vec<i32>>,
 }
