e381e94dbaf97071eea1b72a8cc5628eba9ee2cb
David Waite <d.waite1986@gmail.com>
2023-12-28 21:50:53 +1300

    Refactored lib.rs for new Dice struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use dicecontext::DiceContext;
 use polars::prelude::*;
 use simple_error::bail;
 use std::error::Error;
@@ -5,15 +6,22 @@ use std::fs::File;
 
 mod attackprofile;
 use attackprofile::AttackProfile;
-mod damageelement;
-use damageelement::DamageElement;
 mod dice;
+mod dicecontext;
 
 #[derive(Debug, PartialEq)]
 pub enum HitResult {
     Miss,
     Hit,
     CriticalHit,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Reroll {
+    Standard,
+    Advantage,
+    DoubleAdvantage,
+    Disadvantage,
 }
 
 #[derive(Copy, Clone, Debug, PartialEq)]
@@ -24,9 +32,30 @@ pub enum Ruleset {
 
 //region Private functions
 
-fn bundle_inputs(
+fn produce_attackprofile(
+    target_ac: i32,
+    hit_details: &Vec<String>,
+    weapon_details: &Vec<String>,
+    ruleset: &Ruleset,
+) -> AttackProfile {
+    // Create a vector of AttackProfile structs corresponding to a vector of AC values.
+
+    let hit_context = hit_details
+        .into_iter()
+        .map(|s| DiceContext::parse_dice_string(s))
+        .collect();
+
+    let weapon_context = weapon_details
+        .into_iter()
+        .map(|s| DiceContext::parse_dice_string(s))
+        .collect();
+
+    AttackProfile::new(target_ac, hit_context, weapon_context, *ruleset)
+}
+
+fn map_profiles_to_ac(
     ac_targets: Vec<i32>,
-    to_hit: Vec<i32>,
+    hit_details: Vec<String>,
     weapon_details: Vec<String>,
     ruleset: Ruleset,
 ) -> Vec<AttackProfile> {
@@ -34,22 +63,13 @@ fn bundle_inputs(
 
     let profile_vector: Vec<AttackProfile> = ac_targets
         .into_iter()
-        .map(|ac| {
-            // If struct was changed could create this once and borrow in AttackProfile
-            let damage_elements: Vec<DamageElement> = to_hit
-                .iter()
-                .zip(&weapon_details)
-                .map(|(h, d)| DamageElement::from_notation_string(*h, d))
-                .collect();
-
-            AttackProfile::new(ac, damage_elements, ruleset)
-        })
+        .map(|i| produce_attackprofile(i, &hit_details, &weapon_details, &ruleset))
         .collect();
 
     profile_vector
 }
 
-fn bundle_results(
+fn results_to_dataframe(
     ac_value: i32,
     crit_counter: Vec<i32>,
     hit_counter: Vec<i32>,
@@ -76,7 +96,6 @@ fn bundle_results(
 fn evaluate_attack_profile(attack_profile: AttackProfile, number_turns: i32) -> DataFrame {
     // Simulate a specified number of attack iterations and format the results as a DataFrame.
 
-    //roll_turn(&self, roll_element: &mut ThreadRng) -> (crit_counter, hit_counter, total_damage)
     let mut crit_counter: Vec<i32> = Vec::new();
     let mut hit_counter: Vec<i32> = Vec::new();
     let mut damage_counter: Vec<i32> = Vec::new();
@@ -91,27 +110,12 @@ fn evaluate_attack_profile(attack_profile: AttackProfile, number_turns: i32) -> 
     }
 
     // Bundle results into a DataFrame and return
-    bundle_results(
+    results_to_dataframe(
         attack_profile.target_ac,
         crit_counter,
         hit_counter,
         damage_counter,
     )
-}
-
-fn gather_dataframes(attack_results: Vec<LazyFrame>) -> Result<DataFrame, Box<dyn Error>> {
-    // Compress the results into the final DataFrame for return
-
-    // Documentation on the circumstances that cause the polars concat() function is lacking.
-    // For now just return into the error box until I get a sighting of an error.
-    let concat_result = concat(attack_results, true, true)?;
-
-    let final_df = match concat_result.collect() {
-        Ok(df) => df,
-        _ => bail!("Error collecting individual jobs into single data frame!"),
-    };
-
-    Ok(final_df)
 }
 
 //endregion
@@ -120,28 +124,33 @@ fn gather_dataframes(attack_results: Vec<LazyFrame>) -> Result<DataFrame, Box<dy
 
 pub fn process_simulation(
     ac_targets: Vec<i32>,
-    to_hit: Vec<i32>,
+    hit_details: Vec<String>,
     weapon_details: Vec<String>,
     ruleset: Ruleset,
     number_turns: i32,
-) -> Result<DataFrame, Box<dyn Error>> {
+) -> DataFrame {
     // Partition the inputs over the range of AC values and simulate the attack turns.
 
-    let profile_vector = bundle_inputs(ac_targets, to_hit, weapon_details, ruleset);
+    let profile_vector: Vec<AttackProfile> =
+        map_profiles_to_ac(ac_targets, hit_details, weapon_details, ruleset);
 
-    // Populate a vector of results - later this will dispatch to multiple threads.
-    let mut attack_results: Vec<LazyFrame> = Vec::new();
-    for attack_profile in profile_vector {
-        let ac_df = evaluate_attack_profile(attack_profile, number_turns);
-        attack_results.push(ac_df.lazy());
-    }
+    let attack_results: Vec<LazyFrame> = profile_vector
+        .into_iter()
+        .map(|ap| evaluate_attack_profile(ap, number_turns).lazy())
+        .collect();
 
-    gather_dataframes(attack_results)
+    // Documentation on the circumstances that cause the polars concat() function is lacking.
+    // For now just unwrap and return until I get a sighting of an error, at which point a
+    //  separate function might be required.
+    concat(attack_results, true, true)
+        .unwrap()
+        .collect()
+        .unwrap()
 }
 
 pub fn write_to_parquet(
     output_path: &str,
-    mut file_content: DataFrame,
+    file_content: &mut DataFrame,
 ) -> Result<(), Box<dyn Error>> {
     // Write the DataFrame content into the compressed parquet format.
 
@@ -155,12 +164,29 @@ pub fn write_to_parquet(
 
     // If we get this far, no error was encountered.
     let target_file = create_result.unwrap();
-    match ParquetWriter::new(target_file).finish(&mut file_content) {
+    match ParquetWriter::new(target_file).finish(file_content) {
         Ok(_) => (),
         _ => bail!(error_msg),
     };
 
     Ok(())
+}
+
+pub fn summarise_results(results_df: DataFrame) -> DataFrame {
+    results_df
+        .lazy()
+        .groupby(["Target_AC"])
+        .agg([
+            col("Number_hits").mean().alias("Hits per round (mean)"),
+            col("Number_crits")
+                .mean()
+                .alias("Critical hits per round (mean)"),
+            col("Total_damage").mean().alias("Damage per round (mean)"),
+        ])
+        .sort("Target_AC", Default::default())
+        .rename(["Target_AC"], ["Target AC"])
+        .collect()
+        .unwrap()
 }
 
 //endregion
@@ -169,152 +195,177 @@ pub fn write_to_parquet(
 
 #[cfg(test)]
 mod tests {
+
+    use crate::dice::Die;
+
     use super::*;
     use std::fs;
     use std::path::Path;
 
-    #[test]
-    fn test_bundle_inputs() {
-        // Test the function in a single call over a set of inputs.
+    fn dataframes_are_equal(left_df: DataFrame, right_df: DataFrame) -> () {
+        // Check the shape and column sequence
+        assert_eq!(left_df.shape(), right_df.shape());
+        assert_eq!(left_df.get_column_names(), right_df.get_column_names());
 
-        // Set the input values
-        let input_acs = vec![1, 2];
-        let input_hits = vec![3, 4];
-        let input_details = vec!["1d4".to_string(), "1d6".to_string()];
-
-        // Set the expected output vector
-        let mut exp_profiles: Vec<AttackProfile> = Vec::new();
-        exp_profiles.push(AttackProfile::new(
-            1,
-            vec![
-                DamageElement::from_notation_string(3, "1d4"),
-                DamageElement::from_notation_string(4, "1d6"),
-            ],
-            Ruleset::DND5e,
-        ));
-        exp_profiles.push(AttackProfile::new(
-            2,
-            vec![
-                DamageElement::from_notation_string(3, "1d4"),
-                DamageElement::from_notation_string(4, "1d6"),
-            ],
-            Ruleset::DND5e,
-        ));
-
-        let obs_profiles = bundle_inputs(input_acs, input_hits, input_details, Ruleset::DND5e);
-        assert_eq!(exp_profiles, obs_profiles);
+        // Move through the columns of the left DataFrame, and test that right-hand
+        //  columns match.
+        for column_name in &left_df.get_column_names() {
+            assert_eq!(
+                left_df.column(column_name).unwrap(),
+                right_df.column(column_name).unwrap()
+            );
+        }
     }
 
     #[test]
-    fn test_bundle_results() {
-        // Test the behaviour of the bundle_results() function, assuming no errors.
+    fn test_produce_attackprofile_single() {
+        // Test the function to produce for a single hit/weapon input.
+
+        let exp_ap = AttackProfile::new(
+            10,
+            vec![DiceContext::parse_dice_string("1d4+1")],
+            vec![DiceContext::parse_dice_string("1d10+1")],
+            Ruleset::DND5e,
+        );
+
+        let obs_ap = produce_attackprofile(
+            10,
+            &vec!["1d4+1".to_string()],
+            &vec!["1d10+1".to_string()],
+            &Ruleset::DND5e,
+        );
+
+        assert_eq!(exp_ap, obs_ap);
+    }
+
+    #[test]
+    fn test_produce_attackprofile_multiple() {
+        // Test the function to produce for multiple hit/weapon inputs.
+
+        let exp_ap = AttackProfile::new(
+            10,
+            vec![
+                DiceContext::parse_dice_string("1d4+1"),
+                DiceContext::parse_dice_string("1d6+2"),
+            ],
+            vec![
+                DiceContext::parse_dice_string("1d10+3"),
+                DiceContext::parse_dice_string("1d12+4"),
+            ],
+            Ruleset::DND5e,
+        );
+
+        let obs_ap = produce_attackprofile(
+            10,
+            &vec!["1d4+1".to_string(), "1d6+2".to_string()],
+            &vec!["1d10+3".to_string(), "1d12+4".to_string()],
+            &Ruleset::DND5e,
+        );
+
+        assert_eq!(exp_ap, obs_ap);
+    }
+
+    #[test]
+    fn test_map_profiles_to_ac() {
+        // Test the ability to produce multiple AttackProfiles from a single set of input
+        //  strings to produce the DiceContext structs.
+
+        let exp_aps = vec![
+            AttackProfile::new(
+                10,
+                vec![DiceContext::parse_dice_string("1d4+1")],
+                vec![DiceContext::parse_dice_string("1d12+4")],
+                Ruleset::DND5e,
+            ),
+            AttackProfile::new(
+                15,
+                vec![DiceContext::parse_dice_string("1d4+1")],
+                vec![DiceContext::parse_dice_string("1d12+4")],
+                Ruleset::DND5e,
+            ),
+        ];
+
+        let obs_aps = map_profiles_to_ac(
+            vec![10, 15],
+            vec!["1d4+1".to_string()],
+            vec!["1d12+4".to_string()],
+            Ruleset::DND5e,
+        );
+
+        assert_eq!(exp_aps, obs_aps);
+    }
+
+    #[test]
+    fn test_results_to_dataframe() {
+        // Test the behaviour of the results_to_dataframe() function, assuming no errors.
 
         let input_ac = 5;
         let input_crits = vec![0, 1, 2, 3, 4];
         let input_hits = vec![2, 4, 6, 8, 10];
         let input_damage = vec![10, 12, 14, 16, 18];
 
-        let exp_iteration = Series::new("Iteration", &vec![1, 2, 3, 4, 5]);
-        let exp_ac = Series::new("Target_AC", &vec![5; 5]);
-        let exp_crits = Series::new("Number_crits", &input_crits);
-        let exp_hits = Series::new("Number_hits", &input_hits);
-        let exp_damage = Series::new("Total_damage", &input_damage);
+        let exp_df = df![
+            "Iteration" => &vec![1, 2, 3, 4, 5],
+            "Target_AC" => &vec![5; 5],
+            "Number_hits" => &input_hits,
+            "Number_crits" => &input_crits,
+            "Total_damage" => &input_damage
+        ]
+        .unwrap();
+        let obs_df = results_to_dataframe(input_ac, input_crits, input_hits, input_damage);
 
-        let obs_df = bundle_results(input_ac, input_crits, input_hits, input_damage);
-
-        // Check the shape and contents
-        assert_eq!((5, 5), obs_df.shape());
-        assert_eq!(&exp_iteration, obs_df.column("Iteration").unwrap());
-        assert_eq!(&exp_ac, obs_df.column("Target_AC").unwrap());
-        assert_eq!(&exp_crits, obs_df.column("Number_crits").unwrap());
-        assert_eq!(&exp_hits, obs_df.column("Number_hits").unwrap());
-        assert_eq!(&exp_damage, obs_df.column("Total_damage").unwrap());
+        dataframes_are_equal(exp_df, obs_df);
     }
 
     #[test]
-    fn test_gather_dataframes() {
-        // Test the turnsimulation.gather_dataframes() function for a success case.
+    fn test_evaluate_attack_profile() {
+        // Test the behaviour of the evaluate_attack_profile() function.
+        // Using very carefully controlled dice to have a predictable output.
 
-        let first_segment = df!(
-            "Numbers" => &[1, 2, 3],
-            "Letters" => &["a", "b", "c"],
-        )
-        .unwrap()
-        .lazy();
-        let second_segment = df!(
-            "Numbers" => &[4, 5, 6],
-            "Letters" => &["d", "e", "f"],
-        )
-        .unwrap()
-        .lazy();
-
-        let obs_result = gather_dataframes(vec![first_segment, second_segment]);
-        assert!(obs_result.is_ok());
-
-        // Test the contents of the returned data frame
-        let obs_output = obs_result.unwrap();
-        assert_eq!((6, 2), obs_output.shape());
-        assert_eq!(
-            &Series::new("Numbers", &[1, 2, 3, 4, 5, 6]),
-            obs_output.column("Numbers").unwrap()
+        let attackprofile = AttackProfile::new(
+            1,
+            vec![DiceContext::new(vec![Die::new(2, 3, Reroll::Standard)], 5)],
+            vec![DiceContext::parse_dice_string("1d1+1")],
+            Ruleset::DND5e,
         );
-        assert_eq!(
-            &Series::new("Letters", &["a", "b", "c", "d", "e", "f"]),
-            obs_output.column("Letters").unwrap()
-        );
-    }
 
-    #[test]
-    fn test_gather_dataframes_fail() {
-        // Test the turnsimulation.gather_dataframes() function for a failure case.
+        let exp_df = df![
+            "Iteration" => vec![1, 2, 3, 4, 5],
+            "Target_AC" => vec![1; 5],
+            "Number_hits" => vec![1; 5],
+            "Number_crits" => vec![0; 5],
+            "Total_damage" => vec![2; 5],
+        ]
+        .unwrap();
 
-        let first_segment = df!(
-            "Numbers" => &[1, 2, 3],
-        )
-        .unwrap()
-        .lazy();
-        let second_segment = df!(
-            "Letters" => &["d", "e", "f"],
-        )
-        .unwrap()
-        .lazy();
-
-        let obs_result = gather_dataframes(vec![first_segment, second_segment]);
-        assert!(obs_result.is_err());
-        assert_eq!(
-            "Error collecting individual jobs into single data frame!",
-            obs_result.unwrap_err().to_string()
-        );
+        let obs_df = evaluate_attack_profile(attackprofile, 5);
+        dataframes_are_equal(exp_df, obs_df);
     }
 
     #[test]
     fn test_process_simulation() {
         /* Test the complete run of the turnsimulation.process_simulation() function.
-           Only testing over the success case, as internal errors are tested in each subordinate function, and
-            this just propagates those errors.
+           Only testing over the success case, as internal behaviours are tested in relevant
+            unit tests.
         */
 
-        let obs_result = process_simulation(
-            vec![1, 2, 3],
-            vec![10],
-            vec!["1d4+1".to_string()],
+        let exp_df = df![
+            "Iteration" => vec![1, 2, 3, 4, 5, 1, 2, 3, 4, 5],
+            "Target_AC" => vec![0, 0, 0, 0, 0, 10, 10, 10, 10, 10],
+            "Number_hits" => vec![1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+            "Number_crits" => vec![0; 10],
+            "Total_damage" => vec![2, 2, 2, 2, 2, 0, 0, 0, 0, 0],
+        ]
+        .unwrap();
+
+        let obs_df = process_simulation(
+            vec![0, 10],
+            vec!["1d1+1".to_string()],
+            vec!["1d1+1".to_string()],
             Ruleset::DND5e,
-            2,
+            5,
         );
-
-        // Test the predictable output columns - expecting 3 (AC) repeats of 2 turns.
-        assert!(obs_result.is_ok());
-        let obs_output = obs_result.unwrap();
-
-        assert_eq!((6, 5), obs_output.shape());
-        assert_eq!(
-            &Series::new("Iteration", &[1, 2, 1, 2, 1, 2]),
-            obs_output.column("Iteration").unwrap()
-        );
-        assert_eq!(
-            &Series::new("Target_AC", &[1, 1, 2, 2, 3, 3]),
-            obs_output.column("Target_AC").unwrap()
-        );
+        dataframes_are_equal(exp_df, obs_df);
     }
 
     #[test]
@@ -322,9 +373,9 @@ mod tests {
         // Test the write_to_parquet() function for the success case.
 
         let file_path = "test_write_to_parquet.txt";
-        let df = df!("Temp" => &[1, 2, 3]).unwrap();
+        let mut df = df!("Temp" => &[1, 2, 3]).unwrap();
 
-        let obs_result = write_to_parquet(file_path, df);
+        let obs_result = write_to_parquet(file_path, &mut df);
 
         // Test the return values and that the file exists
         assert!(obs_result.is_ok());
@@ -340,9 +391,9 @@ mod tests {
         // Test the write_to_parquet() function when writing fails (via an invalid path).
 
         let file_path = "bad_path/test_write_to_parquet.txt";
-        let df = df!("Temp" => &[1, 2, 3]).unwrap();
+        let mut df = df!("Temp" => &[1, 2, 3]).unwrap();
 
-        let obs_result = write_to_parquet(file_path, df);
+        let obs_result = write_to_parquet(file_path, &mut df);
 
         // Test the return values
         assert!(obs_result.is_err());
@@ -350,6 +401,30 @@ mod tests {
             "Unable to write to output file path 'bad_path/test_write_to_parquet.txt'!",
             obs_result.unwrap_err().to_string()
         );
+    }
+
+    #[test]
+    fn test_summarise_results() {
+        // Test the behaviour of the summarise_results() function.
+
+        let input_df = df![
+            "Target_AC" => vec![0, 0, 0, 1, 1, 1, 2, 2, 2],
+            "Number_hits" => vec![0, 1, 2, 4, 6, 8, 1, 2, 3],
+            "Number_crits" => vec![0, 1, 2, 4, 6, 8, 1, 2, 3],
+            "Total_damage" => vec![0, 1, 2, 4, 6, 8, 1, 2, 3],
+        ]
+        .unwrap();
+
+        let exp_df = df![
+            "Target AC" => vec![0, 1, 2],
+            "Hits per round (mean)" => vec![1, 6, 2],
+            "Critical hits per round (mean)" => vec![1, 6, 2],
+            "Damage per round (mean)" => vec![1, 6, 2],
+        ]
+        .unwrap();
+
+        let obs_df = summarise_results(input_df);
+        dataframes_are_equal(exp_df, obs_df);
     }
 }
 
