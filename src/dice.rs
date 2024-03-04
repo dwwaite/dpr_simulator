80c6869e832c477360ba565f4a0761db69ac7505
David Waite <d.waite1986@gmail.com>
2024-03-04 20:36:20 +1300

    Fatal dice refactor

--- src/dice.rs
+++ src/dice.rs
@@ -2,45 +2,102 @@ use rand::rngs::ThreadRng;
 use rand::Rng;
 use std::cmp::{max, min};
 
-use crate::Reroll;
+use crate::{DiceMode, Reroll};
 
 #[derive(Debug, PartialEq)]
-pub struct Die {
+pub struct DiceCollection {
+    n_dice: i32,
     roll_min: i32,
     roll_max: i32,
     roll_modifier: Reroll,
+    dice_mode: DiceMode,
+    alt_min: i32,
+    alt_max: i32,
 }
 
-impl Die {
-    pub fn new(roll_min: i32, roll_max: i32, roll_modifier: Reroll) -> Die {
-        Die {
-            roll_min: roll_min,
+impl DiceCollection {
+    pub fn new(n_dice: i32, roll_max: i32, roll_modifier: Reroll) -> DiceCollection {
+        DiceCollection {
+            n_dice: n_dice,
+            roll_min: 1,
             roll_max: roll_max,
             roll_modifier: roll_modifier,
+            dice_mode: DiceMode::Standard,
+            alt_min: -1,
+            alt_max: -1,
         }
     }
 
-    pub fn roll(&self, roll_element: &mut ThreadRng) -> i32 {
-        // Roll the outcome for the Die element.
+    fn make_roll(min: i32, max: i32, roll_element: &mut ThreadRng) -> i32 {
+        // Roll the die element
+        roll_element.gen_range(min..max + 1)
+    }
 
-        match self.roll_modifier {
-            Reroll::Standard => roll_element.gen_range(self.roll_min..self.roll_max + 1),
-            Reroll::Advantage => max(
-                roll_element.gen_range(self.roll_min..self.roll_max + 1),
-                roll_element.gen_range(self.roll_min..self.roll_max + 1),
-            ),
-            Reroll::DoubleAdvantage => *vec![
-                roll_element.gen_range(self.roll_min..self.roll_max + 1),
-                roll_element.gen_range(self.roll_min..self.roll_max + 1),
-                roll_element.gen_range(self.roll_min..self.roll_max + 1),
-            ]
-            .iter()
-            .max()
-            .unwrap(),
-            Reroll::Disadvantage => min(
-                roll_element.gen_range(self.roll_min..self.roll_max + 1),
-                roll_element.gen_range(self.roll_min..self.roll_max + 1),
-            ),
+    fn resolve_roll(roll_min: i32, roll_max: i32, roll_modifier: &Reroll, roll_element: &mut ThreadRng) -> i32 {
+        // Resolve the outcome for the die rolls with regards to advantage mechanics.
+
+        match roll_modifier {
+            Reroll::Standard => DiceCollection::make_roll(roll_min, roll_max, roll_element),
+            Reroll::Advantage => {
+                max(
+                    DiceCollection::make_roll(roll_min, roll_max, roll_element),
+                    DiceCollection::make_roll(roll_min, roll_max, roll_element),
+                )
+            },
+            Reroll::DoubleAdvantage => {
+                *vec![
+                    DiceCollection::make_roll(roll_min, roll_max, roll_element),
+                    DiceCollection::make_roll(roll_min, roll_max, roll_element),
+                    DiceCollection::make_roll(roll_min, roll_max, roll_element),
+                ]
+                .iter()
+                .max()
+                .unwrap()
+            },
+            Reroll::Disadvantage => {
+                min(
+                    DiceCollection::make_roll(roll_min, roll_max, roll_element),
+                    DiceCollection::make_roll(roll_min, roll_max, roll_element),
+                )
+            },
+        }
+    }
+
+    pub fn set_fatal(&mut self, alt_max: i32) {
+        // Change the die statistics for fatal die behaviour.
+
+        self.alt_max = alt_max;
+        self.dice_mode = DiceMode::Fatal;
+    }
+
+    pub fn roll(&self, roll_element: &mut ThreadRng) -> i32 {
+        // Resolve the outcome for the regular die rolls with regards to advantage mechanics and dice behaviour.
+
+        match self.dice_mode {
+            // Currently there are no alternate paths for regular rolls.
+            _ => {
+                let running_total: i32 = (0..self.n_dice)
+                    .map(|_| DiceCollection::resolve_roll(self.roll_min, self.roll_max, &self.roll_modifier, roll_element))
+                    .sum();
+                running_total
+            }
+        }
+    }
+
+    pub fn roll_critical(&self, roll_element: &mut ThreadRng) -> i32 {
+        // Resolve the outcome for the regular die rolls with regards to advantage mechanics and dice behaviour.
+
+        match self.dice_mode {
+            DiceMode::Standard => self.roll(roll_element) + self.roll(roll_element),
+            DiceMode::Fatal => {
+                // Fatal rolls as double dice + 1, at an increased die size.
+                let n_rolls = 2 * self.n_dice + 1;
+                let running_total: i32 = (0..n_rolls)
+                    .map(|_| DiceCollection::resolve_roll(self.roll_min, self.alt_max, &self.roll_modifier, roll_element))
+                    .sum();
+                running_total
+            },
+            _ => 0
         }
     }
 }
@@ -59,77 +116,122 @@ mod tests {
     }
 
     #[test]
-    fn test_roll_standard() {
-        let mut roll_element = rand::thread_rng();
-        let my_die = Die::new(1, 10, Reroll::Standard);
+    fn test_make_roll() {
+        // Test the DiceCollection::make_roll() function, ensuring the minimum and maximum are captured.
 
-        let mut roll_results: Vec<i32> = Vec::new();
-        for _ in 0..10000 {
-            roll_results.push(my_die.roll(&mut roll_element));
-        }
+        let mut roll_element = rand::thread_rng();
+        let roll_results: Vec<i32> = (0..10000).map(|_| DiceCollection::make_roll(1, 10, &mut roll_element)).collect();
 
         let obs_results: (i32, i32) = unpack_roll_vector(&roll_results);
         assert_eq!((1, 10), obs_results);
     }
 
     #[test]
-    fn test_roll_advantage() {
+    fn test_resolve_roll_standard() {
+        // Test the DiceCollection::resolve_roll() function for Standard dice roll mechanics.
+
         let mut roll_element = rand::thread_rng();
-        let std_die = Die::new(1, 10, Reroll::Standard);
-        let adv_die = Die::new(1, 10, Reroll::Advantage);
+        let roll_results: Vec<i32> = (0..10000).map(|_| DiceCollection::resolve_roll(1, 10, &Reroll::Standard, &mut roll_element)).collect();
 
-        let mut std_results: Vec<i32> = Vec::new();
-        let mut adv_results: Vec<i32> = Vec::new();
-        for _ in 0..10000 {
-            std_results.push(std_die.roll(&mut roll_element));
-            adv_results.push(adv_die.roll(&mut roll_element));
-        }
+        let obs_results: (i32, i32) = unpack_roll_vector(&roll_results);
+        assert_eq!((1, 10), obs_results);
+    }
 
-        // Take the sum of both vectors. Expectation is that the die with
-        //  advantage will sum to greater than standard die.
+    #[test]
+    fn test_resolve_roll_advantage() {
+        // Test the DiceCollection::resolve_roll() function for Advantage dice roll mechanics.
+        // Essentially a confirmation that on average Advantage roll is higher than Standard rolls.
+
+        let mut roll_element = rand::thread_rng();
+        let std_results: Vec<i32> = (0..10000).map(|_| DiceCollection::resolve_roll(1, 10, &Reroll::Standard, &mut roll_element)).collect();
+        let adv_results: Vec<i32> = (0..10000).map(|_| DiceCollection::resolve_roll(1, 10, &Reroll::Advantage, &mut roll_element)).collect();
+
         let std_total: i32 = std_results.iter().sum();
         let adv_total: i32 = adv_results.iter().sum();
         assert!(adv_total > std_total);
     }
 
     #[test]
-    fn test_roll_disadvantage() {
+    fn test_resolve_roll_doubleadvantage() {
+        // Test the DiceCollection::resolve_roll() function for DoubleAdvantage dice roll mechanics.
+        // Essentially a confirmation that on average DoubleAdvantage roll is higher than Advantage rolls.
+
         let mut roll_element = rand::thread_rng();
-        let std_single = Die::new(1, 10, Reroll::Standard);
-        let adv_die = Die::new(1, 10, Reroll::Disadvantage);
+        let adv_results: Vec<i32> = (0..10000).map(|_| DiceCollection::resolve_roll(1, 10, &Reroll::Advantage, &mut roll_element)).collect();
+        let dbl_results: Vec<i32> = (0..10000).map(|_| DiceCollection::resolve_roll(1, 10, &Reroll::DoubleAdvantage, &mut roll_element)).collect();
 
-        let mut std_results: Vec<i32> = Vec::new();
-        let mut adv_results: Vec<i32> = Vec::new();
-        for _ in 0..10000 {
-            std_results.push(std_single.roll(&mut roll_element));
-            adv_results.push(adv_die.roll(&mut roll_element));
-        }
-
-        // Take the sum of both vectors. Expectation is that the die with
-        //  disadvantage will sum to less than standard die.
-        let std_total: i32 = std_results.iter().sum();
         let adv_total: i32 = adv_results.iter().sum();
-        assert!(adv_total < std_total);
+        let double_total: i32 = dbl_results.iter().sum();
+        assert!(double_total > adv_total);
     }
 
     #[test]
-    fn test_roll_doubleadvantage() {
+    fn test_resolve_roll_disadvantage() {
+        // Test the DiceCollection::resolve_roll() function for Disadvantage dice roll mechanics.
+        // Essentially a confirmation that on average Disadvantage roll is lower than Standard rolls.
+
         let mut roll_element = rand::thread_rng();
-        let adv_die = Die::new(1, 10, Reroll::Advantage);
-        let double_die = Die::new(1, 10, Reroll::DoubleAdvantage);
+        let std_results: Vec<i32> = (0..10000).map(|_| DiceCollection::resolve_roll(1, 10, &Reroll::Advantage, &mut roll_element)).collect();
+        let dis_results: Vec<i32> = (0..10000).map(|_| DiceCollection::resolve_roll(1, 10, &Reroll::Disadvantage, &mut roll_element)).collect();
 
-        let mut adv_results: Vec<i32> = Vec::new();
-        let mut double_results: Vec<i32> = Vec::new();
-        for _ in 0..10000 {
-            adv_results.push(adv_die.roll(&mut roll_element));
-            double_results.push(double_die.roll(&mut roll_element));
-        }
+        let std_total: i32 = std_results.iter().sum();
+        let dis_total: i32 = dis_results.iter().sum();
+        assert!(dis_total < std_total);
+    }
 
-        // Take the sum of both vectors. Expectation is that the die with
-        //  double-advantage will sum to greater than the advantage die.
-        let adv_total: i32 = adv_results.iter().sum();
-        let double_total: i32 = double_results.iter().sum();
-        assert!(double_total > adv_total);
+    #[test]
+    fn test_set_fatal() {
+        // Test the DiceCollection::set_fatal() function.
+
+        let mut dc = DiceCollection::new(1, 6, Reroll::Standard);
+        dc.set_fatal(8);
+
+        assert_eq!(dc.alt_max, 8);
+        assert_eq!(dc.dice_mode, DiceMode::Fatal);
+    }
+
+    #[test]
+    fn test_roll_standard() {
+        // Test the DiceCollection::roll() function for default behaviour.
+        // This is basically the instanced wrapper of the DiceCollection::resolve_roll() function
+        //  so only testing on standard behaviour.
+
+        let dc = DiceCollection::new(1, 10, Reroll::Standard);
+        let mut roll_element = rand::thread_rng();
+
+        let results: Vec<i32> = (0..10000).map(|_| dc.roll(&mut roll_element)).collect();
+        let (obs_min, obs_max) = unpack_roll_vector(&results);
+        assert_eq!(1, obs_min);
+        assert_eq!(10, obs_max);
+    }
+
+    #[test]
+    fn test_critical_roll_standard() {
+        // Test the DiceCollection::roll_critical() function for standard behaviour.
+        // Expected roll values are double the minimum and maximum per die rolled.
+
+        let dc = DiceCollection::new(1, 10, Reroll::Standard);
+        let mut roll_element = rand::thread_rng();
+
+        let results: Vec<i32> = (0..10000).map(|_| dc.roll_critical(&mut roll_element)).collect();
+        let (obs_min, obs_max) = unpack_roll_vector(&results);
+        assert_eq!(2, obs_min);
+        assert_eq!(20, obs_max);
+    }
+
+    #[test]
+    fn test_critical_roll_fatal() {
+        // Test the DiceCollection::roll_critical() function for fatal behaviour.
+        // Expected roll values are 2x + 1 for minimum and maximum.
+
+        let mut dc = DiceCollection::new(1, 10, Reroll::Standard);
+        dc.set_fatal(12);
+
+        let mut roll_element = rand::thread_rng();
+        let results: Vec<i32> = (0..10000).map(|_| dc.roll_critical(&mut roll_element)).collect();
+        let (obs_min, obs_max) = unpack_roll_vector(&results);
+        assert_eq!(3, obs_min);
+        assert_eq!(36, obs_max);
     }
 }
 
--- src/dicecontext.rs
+++ src/dicecontext.rs
@@ -1,61 +1,63 @@
 use rand::rngs::ThreadRng;
 use regex::Regex;
 
-use crate::{dice::Die, Reroll};
+use crate::{dice::DiceCollection, Reroll};
 
 const ROLL_MIN: i32 = 1;
 
 #[derive(Debug, PartialEq)]
 pub struct DiceContext {
-    dice: Vec<Die>,
+    dice: Vec<DiceCollection>,
     pub static_modifier: i32,
 }
 
 impl DiceContext {
-    pub fn new(dice: Vec<Die>, static_modifier: i32) -> DiceContext {
+    pub fn new(dice: Vec<DiceCollection>, static_modifier: i32) -> DiceContext {
         DiceContext {
             dice: dice,
             static_modifier: static_modifier,
         }
     }
 
-    fn parse_die_elements(notation: &str) -> (i32, i32) {
+    fn parse_regular_die(notation: &str) -> Option<(i32, i32)> {
         // Return a tuple representing the number of die and size, extracted from
         // the notation string.
 
         // The regex can only fail to compile on a system error, so safe to unwrap.
-        let regex_die = Regex::new(r"\d+d\d+").unwrap();
+        let regex_regular = Regex::new(r"\d+d\d+").unwrap();
 
-        let die_elements: (i32, i32) = match regex_die.find(notation) {
+        match regex_regular.find(notation) {
+    
             Some(ref x) => {
                 let tokens: Vec<&str> = x.as_str().split("d").collect();
                 let n_die = tokens[0].parse().unwrap();
                 let s_die = tokens[1].parse().unwrap();
-
-                (n_die, s_die)
+    
+                Some((n_die, s_die))
             }
-            None => (0, 0),
-        };
-
-        die_elements
+            None => None,
+        }
     }
 
-    fn parse_reroll_elements(notation: &str) -> Reroll {
-        // Set the reroll flag - standard roll, roll with advantage, or roll
-        // with disadvantage.
+    fn parse_fatal_die(notation: &str) -> Option<(i32, i32, i32)> {
+        // Return a tuple representing the number of die, standard size, and fatal size
+        //  extracted from the notation string.
 
         // The regex can only fail to compile on a system error, so safe to unwrap.
-        let regex_reroll = Regex::new(r"AA|A|D").unwrap();
-        let value_capture: &str = match regex_reroll.find(notation) {
-            Some(x) => x.as_str(),
-            None => "",
-        };
+        let regex_fatal = Regex::new(r"(?<f>\d+f\d+\~\d+)").unwrap();
+        let regex_split = Regex::new(r"f|\~").unwrap();
 
-        match value_capture {
-            "AA" => Reroll::DoubleAdvantage,
-            "A" => Reroll::Advantage,
-            "D" => Reroll::Disadvantage,
-            _ => Reroll::Standard,
+        match regex_fatal.find(notation) {
+
+            Some(ref x) => {
+                let string_capture: &str = x.as_str();
+                let tokens: Vec<i32> = regex_split.split(string_capture)
+                    .map(|x| x.parse().unwrap())
+                    .collect();
+    
+                Some((tokens[0], tokens[1], tokens[2]))
+            }
+            None => None,
         }
     }
 
@@ -78,32 +80,54 @@ impl DiceContext {
         value_capture
     }
 
+    fn parse_reroll_elements(notation: &str) -> Reroll {
+        // Set the reroll flag - standard roll, roll with advantage, or roll
+        // with disadvantage.
+
+        // The regex can only fail to compile on a system error, so safe to unwrap.
+        let regex_reroll = Regex::new(r"AA|A|D").unwrap();
+        let value_capture: &str = match regex_reroll.find(notation) {
+            Some(x) => x.as_str(),
+            None => "",
+        };
+
+        match value_capture {
+            "AA" => Reroll::DoubleAdvantage,
+            "A" => Reroll::Advantage,
+            "D" => Reroll::Disadvantage,
+            _ => Reroll::Standard,
+        }
+    }
+
     pub fn parse_dice_string(notation: &str) -> DiceContext {
         // Parse the notation string into individual die notations, and the static modifier.
 
-        // Parse the individual die in the notation string.
-        let die_vector: Vec<Die> = notation
-            .split(",")
-            .map(|n| {
-                let (n_die, die_size) = DiceContext::parse_die_elements(n);
+        let mut dice_vector: Vec<DiceCollection> = Vec::new();
 
-                let mut die_vector: Vec<Die> = Vec::new();
-                for _ in 0..n_die {
-                    die_vector.push(Die::new(
-                        ROLL_MIN,
-                        die_size,
-                        DiceContext::parse_reroll_elements(n),
-                    ));
-                }
-                die_vector
-            })
-            .into_iter()
-            .flatten()
-            .collect();
-
+        // Extract the recycling elements
         let static_modifier: i32 = DiceContext::parse_static_elements(notation);
 
-        DiceContext::new(die_vector, static_modifier)
+        // Parse the die from the notation string.
+        for n in notation.split(",") {
+
+            if let Some((n_die, die_size)) = DiceContext::parse_regular_die(n) {
+                // If a standard (1d4) notation is found, store a base die collection.
+
+                let reroll_modifier = DiceContext::parse_reroll_elements(n);
+                dice_vector.push(DiceCollection::new(n_die, die_size, reroll_modifier));
+
+            } else if let Some((n_die, die_size, fatal_size)) = DiceContext::parse_fatal_die(n) {
+                // If a fatal (1f4~6) notation is found, store as a die with the Fatal behaviour.
+
+                let reroll_modifier = DiceContext::parse_reroll_elements(n);
+                let mut f_dice = DiceCollection::new(n_die, die_size, reroll_modifier);
+                f_dice.set_fatal(fatal_size);
+
+                dice_vector.push(f_dice);
+            }
+        }
+
+        DiceContext::new(dice_vector, static_modifier)
     }
 
     pub fn roll(&self, roll_element: &mut ThreadRng) -> i32 {
@@ -112,7 +136,15 @@ impl DiceContext {
         self.dice
             .iter()
             .map(|d| d.roll(roll_element))
-            .into_iter()
+            .sum()
+    }
+
+    pub fn roll_critical(&self, roll_element: &mut ThreadRng) -> i32 {
+        // Roll the outcome for the DiceContext element.
+
+        self.dice
+            .iter()
+            .map(|d| d.roll_critical(roll_element))
             .sum()
     }
 }
@@ -125,40 +157,50 @@ mod tests {
 
     #[test]
     fn test_parse_die_elements() {
-        let exp_vector: (i32, i32) = (1, 8);
-        let obs_vector: (i32, i32) = DiceContext::parse_die_elements("1d8+5");
+        // Test the DiceContext.parse_regular_die() on a standard notation.
+
+        let exp_vector = Some((1, 8));
+        let obs_vector = DiceContext::parse_regular_die("1d8+5");
 
         assert_eq!(exp_vector, obs_vector);
     }
 
     #[test]
     fn test_parse_die_elements_none() {
-        let exp_vector: (i32, i32) = (0, 0);
-        let obs_vector: (i32, i32) = DiceContext::parse_die_elements("+5");
+        // Test the DiceContext.parse_regular_die() on a string with no die notation.
 
-        assert_eq!(exp_vector, obs_vector);
+        let obs_vector = DiceContext::parse_regular_die("+5");
+        assert_eq!(None, obs_vector);
     }
 
     #[test]
     fn test_parse_static_elements_single_pos() {
+    // Test the DiceContext.parse_static_elements() function for a positive element.
+
         let obs_result: i32 = DiceContext::parse_static_elements("1d8+5");
         assert_eq!(5, obs_result);
     }
 
     #[test]
     fn test_parse_static_elements_single_neg() {
+        // Test the DiceContext.parse_static_elements() function for a negative element.
+
         let obs_result: i32 = DiceContext::parse_static_elements("1d8-5");
         assert_eq!(-5, obs_result);
     }
 
     #[test]
     fn test_parse_static_elements_multiple() {
+        // Test the DiceContext.parse_static_elements() function for mixed elements.
+
         let obs_result: i32 = DiceContext::parse_static_elements("1d8+5-3");
         assert_eq!(2, obs_result);
     }
 
     #[test]
     fn test_parse_reroll_elements_single_a() {
+        // Test the DiceContext.parse_reroll_elements() function for Advantage strings.
+
         for s in vec!["1d4A", "A1d4"] {
             let obs_result: Reroll = DiceContext::parse_reroll_elements(s);
             assert_eq!(Reroll::Advantage, obs_result);
@@ -167,6 +209,8 @@ mod tests {
 
     #[test]
     fn test_parse_reroll_elements_single_aa() {
+        // Test the DiceContext.parse_reroll_elements() function for DoubleAdvantage strings.
+
         for s in vec!["1d4AA", "AA1d4"] {
             let obs_result: Reroll = DiceContext::parse_reroll_elements(s);
             assert_eq!(Reroll::DoubleAdvantage, obs_result);
@@ -175,6 +219,8 @@ mod tests {
 
     #[test]
     fn test_parse_reroll_elements_single_d() {
+        // Test the DiceContext.parse_reroll_elements() function for Disadvantage strings.
+
         for s in vec!["1d4D", "D1d4"] {
             let obs_result: Reroll = DiceContext::parse_reroll_elements(s);
             assert_eq!(Reroll::Disadvantage, obs_result);
@@ -183,6 +229,8 @@ mod tests {
 
     #[test]
     fn test_parse_reroll_elements_mixed_a() {
+        // Test the DiceContext.parse_reroll_elements() function for mixed strings with Advantage first.
+
         for s in vec!["A1d4D", "AD1d4"] {
             let obs_result: Reroll = DiceContext::parse_reroll_elements(s);
             assert_eq!(Reroll::Advantage, obs_result);
@@ -191,6 +239,8 @@ mod tests {
 
     #[test]
     fn test_parse_reroll_elements_mixed_d() {
+        // Test the DiceContext.parse_reroll_elements() function for mixed strings with Disadvantage first.
+
         for s in vec!["D1d4A", "DA1d4"] {
             let obs_result: Reroll = DiceContext::parse_reroll_elements(s);
             assert_eq!(Reroll::Disadvantage, obs_result);
@@ -199,40 +249,89 @@ mod tests {
 
     #[test]
     fn test_parse_reroll_elements_default() {
+        // Test the DiceContext.parse_reroll_elements() function.
+
         let obs_result: Reroll = DiceContext::parse_reroll_elements("1d4");
         assert_eq!(Reroll::Standard, obs_result);
     }
 
     #[test]
-    fn test_parse_dice_string() {
+    fn test_parse_dice_string_standard() {
+        // Test the DiceContext.parse_dice_string() function for regular die only, covering
+        //  all advantage states.
+
         let exp_context = DiceContext::new(
             vec![
-                Die::new(1, 4, Reroll::Standard),
-                Die::new(1, 6, Reroll::Disadvantage),
-                Die::new(1, 6, Reroll::Disadvantage),
+                DiceCollection::new(1, 4, Reroll::Standard),
+                DiceCollection::new(2, 6, Reroll::Disadvantage),
+                DiceCollection::new(3, 6, Reroll::Advantage),
+                DiceCollection::new(4, 6, Reroll::DoubleAdvantage),
             ],
             7,
         );
 
-        let obs_context: DiceContext = DiceContext::parse_dice_string("1d4,D2d6+7");
+        let obs_context: DiceContext = DiceContext::parse_dice_string("1d4,D2d6,3d6A,4d6AA+7");
+        assert_eq!(exp_context, obs_context);
+    }
+
+    #[test]
+    fn test_parse_dice_string_fatal() {
+        // Test the DiceContext.parse_dice_string() function for a regular die and fatal die, for only
+        //  standard reroll states.
+
+        let mut f_die = DiceCollection::new(2, 6, Reroll::Standard);
+        f_die.set_fatal(8);
+
+        let exp_context = DiceContext::new(
+            vec![
+                DiceCollection::new(1, 4, Reroll::Standard),
+                f_die
+            ],
+            7,
+        );
+
+        let obs_context: DiceContext = DiceContext::parse_dice_string("1d4,2f6~8+7");
         assert_eq!(exp_context, obs_context);
     }
 
     #[test]
     fn test_roll() {
+        // Test the DiceContext.roll() function. Behaviour is only for regular die, as other behaviours
+        //  are tested within the DiceCollection struct.
+
         let mut roll_element = rand::thread_rng();
         let context = DiceContext::new(
             vec![
-                Die::new(1, 4, Reroll::Standard),
-                Die::new(1, 4, Reroll::Standard),
+                DiceCollection::new(1, 4, Reroll::Standard),
+                DiceCollection::new(2, 6, Reroll::Standard),
             ],
             20,
         );
 
-        // Total of the roll must be at least 1 + 1 but less than the static modifier
+        // Total of the roll must be greater than (3 * die, with minimum size each), but less than the static modifier
         let obs_result = context.roll(&mut roll_element);
-        assert!(obs_result >= 2);
+        assert!(obs_result >= 3);
         assert!(obs_result < 20);
+    }
+
+    #[test]
+    fn test_roll_critical() {
+        // Test the DiceContext.roll_critical() function. Behaviour is only for regular die, as other behaviours
+        //  are tested within the DiceCollection struct.
+
+        let mut roll_element = rand::thread_rng();
+        let context = DiceContext::new(
+            vec![
+                DiceCollection::new(1, 4, Reroll::Standard),
+                DiceCollection::new(2, 6, Reroll::Standard),
+            ],
+            100,
+        );
+
+        // Total of the roll must be greater than (2 * 3 * die, with minimum size each), but less than the static modifier
+        let obs_result = context.roll_critical(&mut roll_element);
+        assert!(obs_result >= 6);
+        assert!(obs_result < 100);
     }
 }
 
